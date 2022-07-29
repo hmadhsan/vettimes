ea38c6915400fcc21e455c6454bb123724feb897
tahamir999 <taha.mir@qavitech.com>
2022-07-29 17:08:27 +0500

    cpd url changed to staging

--- config/constants.js
+++ config/constants.js
@@ -1,3 +1,3 @@
 // export const cpdBaseUrl = `http://cpdlocal.vettimes.co.uk:3000` //FOR LOCAL
 export const cpdBaseUrl = `https://cpd-staging.vettimes.co.uk` //FOR STAGING
-export const BASE_URL = `https://cpd.vettimes.co.uk/`
+export const BASE_URL = `https://cpd-staging.vettimes.co.uk/`
